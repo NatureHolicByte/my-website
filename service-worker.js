eb8559bfabbc29a527d380dd97dd41232582ae34
Abhipraya A <apraya.aprama@gmail.com>
2025-07-07 14:44:58 +0700

    Initial commit

---
+++ service-worker.js
@@ -0,0 +1,38 @@
+const CACHE_NAME = "bday-site-v2"; // Ganti ini kalau update file
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./letter.html",
+  "./gallery.html",
+  "./manifest.json"
+];
+
+self.addEventListener("install", (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then((cache) => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  // Hapus cache lama
+  event.waitUntil(
+    caches.keys().then((cacheNames) =>
+      Promise.all(
+        cacheNames.map((name) => {
+          if (name !== CACHE_NAME) {
+            return caches.delete(name);
+          }
+        })
+      )
+    )
+  );
+});
+
+self.addEventListener("fetch", (event) => {
+  event.respondWith(
+    caches.match(event.request).then((response) =>
+      response || fetch(event.request)
+    )
+  );
+});
